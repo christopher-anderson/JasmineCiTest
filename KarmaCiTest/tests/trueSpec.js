af54c2e80063c77eca797ff67fea2795832f5a53
Christopher Anderson <christopher.bl.anderson@gmail.com>
2015-07-09 16:47:52 -0500

    Initial Commit

---
+++ KarmaCiTest/tests/trueSpec.js
@@ -0,0 +1,5 @@
+describe("A suite", function(){
+    it("contains spec with an expectation of true", function(){
+        expect(returnTrue()).toBe(true);
+    })
+});
---
+++ KarmaCiTest/true.js
@@ -0,0 +1,6 @@
+/**
+ * Created by andersonc1 on 7/9/15.
+ */
+function returnTrue(){
+    return true;
+}
